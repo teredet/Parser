367fd35c9b1df70a0574a19e30e29056d93e8480
teredet <artem.vanzytler@gmail.com>
2021-11-10 12:48:21 +0200

    Add parsing from djinni.co. Information is collected from all pages, unlike dou.ua

--- parser.py
+++ parser.py
@@ -30,16 +30,57 @@ def get_dou_vacancy():
         })
     return res
 
+
+def get_djinni_vacancy():    
+    
+    def get_info_from_page(url_page):
+        html = get_html(url_page)
+        soup = BeautifulSoup(html, 'lxml')
+        vacancy = soup.find_all('li', class_='list-jobs__item')
+
+
+        for i in vacancy:
+            title = i.find('a', class_='profile').get_text()
+            desc = i.find('div', class_='list-jobs__description').get_text(strip=True).replace('\r\n', '')
+            href = 'https://djinni.co/' + i.find('a', class_='profile').get('href')
+            res.append({
+                'title': title,
+                'desc': desc,
+                # 'details':details,
+                'href': href,
+            })
+
+
+        return soup.find('ul', class_='pager').find_all('a')[-1].text
+    
+    x = 1
+    res = []
+    page_url = 'https://djinni.co/jobs/keyword-python/kyiv/?exp_level=no_exp'
+    while True:
+        if get_info_from_page(page_url) != 'наступна →':
+            return res
+        x += 1
+        get_info_from_page(f'https://djinni.co/jobs/keyword-python/kyiv/?exp_level=no_exp&page={x}')
+        page_url = f'https://djinni.co/jobs/keyword-python/kyiv/?exp_level=no_exp&page={x}'
+
+
+
 def main():
     dou_vacancy = get_dou_vacancy()
+    djinni_vacancy = get_djinni_vacancy()
 
     with open('vacancy.txt', 'w', encoding='utf-8') as f:   
         i=1
-        f.write('From dou.ua\n\n')
+        f.write('\n\nFrom dou.ua\n\n')
         for item in dou_vacancy:
             f.write(f'{i}. {item["title"]}\n {item["desc"]}\n {item["href"]}\n\n')
             i += 1
-        f.write('------------------------------------------------------------------------------------')
+        f.write('-'*50)
+        f.write('\n\nFrom djinni.co\n\n')
+        for item in djinni_vacancy:
+            f.write(f'{i}. {item["title"]}\n {item["desc"]}\n {item["href"]}\n\n')
+            i += 1
+        f.write('-'*50)
 
 
 if __name__ == '__main__':
